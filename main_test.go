79fa0d6a450489b8d17bf7b118f31a5d4cacd88e
Julio Sousa <julioams@icloud.com>
2025-04-01 21:22:56 -0300

    ✅ (main_test.go): add additional test case to cover the scenario when an empty string is supplied to the Hello function

--- main_test.go
+++ main_test.go
@@ -3,10 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	result := Hello("Chris")
-	expected := "Hello, Chris!"
+	t.Run("saying hello to people", func(t *testing.T) {
+		result := Hello("Chris")
+		expected := "Hello, Chris!"
 
-	if result != expected {
-		t.Errorf("Hello() = %q; expected %q", result, expected)
-	}
+		if result != expected {
+			t.Errorf("result %q; expected %q", result, expected)
+		}
+	})
+
+	t.Run("say 'Hello, World!' when an empty string is supplied", func(t *testing.T) {
+		result := Hello("")
+		expected := "Hello, World!"
+
+		if result != expected {
+			t.Errorf("result %q; expected %q", result, expected)
+		}
+	})
 }

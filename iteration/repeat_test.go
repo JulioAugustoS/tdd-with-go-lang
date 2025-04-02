32c9cd4cf367208d1edd1aa289c05716baed6d5f
Julio Sousa <julioams@icloud.com>
2025-04-01 22:17:05 -0300

    ✅ (repeat_test.go): add benchmark test for Repeat function to measure performance 📝 (repeat_test.go): add Example test for Repeat function to demonstrate usage and expected output

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,6 +1,9 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
 	repeated := Repeat("a", 5)
@@ -9,4 +12,16 @@ func TestRepeat(t *testing.T) {
 	if repeated != expected {
 		t.Errorf("expected '%q' but got '%q'", expected, repeated)
 	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("a", 5)
+	fmt.Println(repeated)
+	// Output: aaaaa
 }

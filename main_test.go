437b9a9b970a5fb02741fbbb6d66dab0c5ea1163
Julio Sousa <julioams@icloud.com>
2025-04-01 21:26:58 -0300

    ✅ (main.go): add input validation to set default name to "World" if empty string is provided ♻️ (main_test.go): refactor test cases to use a helper function for verifying correct message output

--- main_test.go
+++ main_test.go
@@ -3,21 +3,25 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
+	verifyCorrectMessage := func(t *testing.T, result, expected string) {
+		t.Helper()
+		if result != expected {
+			t.Errorf("result %q; expected %q", result, expected)
+		}
+	}
+
+
 	t.Run("saying hello to people", func(t *testing.T) {
 		result := Hello("Chris")
 		expected := "Hello, Chris!"
 
-		if result != expected {
-			t.Errorf("result %q; expected %q", result, expected)
-		}
+		verifyCorrectMessage(t, result, expected)
 	})
 
 	t.Run("say 'Hello, World!' when an empty string is supplied", func(t *testing.T) {
 		result := Hello("")
 		expected := "Hello, World!"
 
-		if result != expected {
-			t.Errorf("result %q; expected %q", result, expected)
-		}
+		verifyCorrectMessage(t, result, expected)
 	})
 }
